96a840f959c10d9b3b10d3e1b69f739bfe13559b
jdmesalosada <jdmesalosada@gmail.com>
2017-07-29 15:19:00 -0500

    moved files

--- spec/screenplay/questions/home/homeQuestions.ts
+++
@@ -1,8 +0,0 @@
-import { Question, Text } from 'serenity-js/lib/screenplay-protractor';
-import { HomePage } from '../../UI/homePage';
-
-export class HomeQuestions {
-
-    static LogOutText: Question<PromiseLike<string>> = Text.of(HomePage.LogOut);
-
-}
