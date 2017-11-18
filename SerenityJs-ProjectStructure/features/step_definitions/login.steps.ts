d6a42fe0d5db52d58e61c6072cd71450b50cab41
jdmesalosada <jdmesalosada@gmail.com>
2017-11-18 11:31:31 -0500

    added serenity project structure

---
+++ SerenityJs-ProjectStructure/features/step_definitions/login.steps.ts
@@ -0,0 +1,15 @@
+export = function loginSteps() {
+
+    this.Given(/^that James opens the Login page$/, function () {
+
+    });
+
+    this.When(/^he enters a wrong credentials$/, function () {
+        
+    });
+
+    this.Then(/^he should be warned about the invalid credentials$/, function () {
+        
+    });
+
+}
