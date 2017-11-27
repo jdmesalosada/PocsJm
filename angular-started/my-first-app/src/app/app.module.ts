8e207856b38a09ea150eaa40be1b9d3072b7f56d
jdmesalosada <jdmesalosada@gmail.com>
2017-11-27 18:45:58 -0500

    added components

--- angular-started/my-first-app/src/app/app.module.ts
+++ angular-started/my-first-app/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
