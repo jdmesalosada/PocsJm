79b7ed46d8456315e6fb465902c477780a20afdf
Julian Mesa <mesaj@colpatria.com>
2017-12-04 13:23:46 -0500

    ngclass

--- angular-started/my-first-app/src/app/server/server.component.ts
+++ angular-started/my-first-app/src/app/server/server.component.ts
@@ -3,12 +3,21 @@ import { Component } from '@angular/core';
 @Component({
     'selector': 'app-server',
     'templateUrl': './server.component.html',
+    styles: [`
+    .online {
+      color: white;`
+    ]
 })
 export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = 'offLine';
 
     getServerStatus() {
-        return this.serverStatus;
+        return this.serverStatus =
+            Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
