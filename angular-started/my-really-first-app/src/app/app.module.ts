eebe2819b2f130c1e2f7fa8432c10f3f67406ab2
Julian Mesa <mesaj@colpatria.com>
2017-12-07 13:32:11 -0500

    added recipe list and recipe detail components

--- angular-started/my-really-first-app/src/app/app.module.ts
+++ angular-started/my-really-first-app/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
