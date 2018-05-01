51144a77209fc85c429dcda35321cd58e4a699a5
Julian Mesa <julianmesa@Julians-MacBook-Air.local>
2018-04-30 20:07:21 -0500

    management post

--- node_api_design/version1/index.js
+++ node_api_design/version1/index.js
@@ -1,6 +1,17 @@
 const express = require('express'); //return a funcion
 const app = express(); //return an object
 
+//Habilita el parseo de objetos json en express,
+//por defecto esta caracteristica viene apagada por lo que usando 
+//la siguient linea la prendemos.
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: 'course1' },
+  { id: 2, name: 'course2' },
+  { id: 3, name: 'course3' }
+];
+
 //this function requires two paramters, the path
 // and a callback that is going to be executed when the path is called.
 app.get('/', (req, res) => {
@@ -8,12 +19,22 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/courses', (req, res) => {
-  res.send(['1,2,3,4']);
+  res.send(courses);
 });
 
 //id va a ser el nombre del parametro que vamos a esperar.
-app.get('/api/courses/:id', (req, res) => {
+/*app.get('/api/courses/:id', (req, res) => {
      res.send(req.params.id);
+});*/
+
+
+app.get('/api/courses/:id', (req, res) => {
+  //La funcion parseInt es una funcion global de javascript.
+  const courseId = req.params.id;
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  //if there is not any course we return: 404
+  if (!course) res.status(404).send(`The course with the given ID ${courseId} was not found.`);
+  res.send(course);
 });
 
 //si recibimos multiples parametros en la url sería
@@ -36,4 +57,25 @@ const port = process.env.PORT || 3000;
 //este escuchando por el puerto
 app.listen(port, () => {
   console.log(`Listening on port ${port} ...`);
+});
+
+
+/******############# */
+//POST #
+/******############## */
+
+app.post('/api/courses', (req, res) => {
+
+  if(!req.body.name || req.body.name < 3)
+  {
+    //Si no viene el nombre o su longitud es menor a 3 devolvemos un 400 que es la convención
+    //de Bad request
+    
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  }
+  courses.push(course);
+  res.send(course);
 });
