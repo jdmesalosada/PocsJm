983e4b4ac13499c8e351aa45087aa1909846ba84
Julian Mesa <julianmesa@Julians-MacBook-Air.local>
2018-05-05 17:49:31 -0500

    added running promises parallel

---
+++ node_api_design/async-demo/promises-parallel.js
@@ -0,0 +1,17 @@
+const p1 = new Promise((resolve) => {
+    setTimeout(()=> {
+        console.log('Asynchronous operation 1');        
+        resolve(1);
+    }, 2000);
+});
+
+const p2 = new Promise((resolve) => {
+    setTimeout(()=> {
+        console.log('Asynchronous operation 2');        
+        resolve(2);
+    }, 2000);
+});
+
+
+Promise.all([p1, p2])
+    .then(result => console.log(result) );
