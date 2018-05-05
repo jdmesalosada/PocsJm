cc03eb709c8600bc1fb219ce0e4498053b77d856
Julian Mesa <julianmesa@Julians-MacBook-Air.local>
2018-05-05 17:34:01 -0500

    added asynchronous operations

--- node_api_design/async-demo/promise-api.js
+++ node_api_design/async-demo/promise-api.js
@@ -1,5 +1,5 @@
 const p = Promise.resolve({id:1});
 p.then(result => console.log(result));
 
-const pReject = Promise.reject('error message');
+const pReject = Promise.reject(new Error('error message'));
 pReject.catch(err => console.log(err.message));
