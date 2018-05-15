346a09d6aaee4563bb72c89d812987a89e9b287e
Julian Mesa <julianmesa@Julians-MacBook-Air.local>
2018-05-15 18:42:01 -0500

    added subdocument

--- node_api_design/mongo-demo3-referencing-documents/embedding.js
+++ node_api_design/mongo-demo3-referencing-documents/embedding.js
@@ -14,7 +14,10 @@ const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
   name: String,
-  author: authorSchema
+  author:{
+    type: authorSchema,
+    required: true
+  } 
 }));
 
 async function createCourse(name, author) {
@@ -38,5 +41,15 @@ async function updateAuthor(courseId){
   course.save();
 }
 
+//update directly in the database
+async function updateAuthor2(courseId){
+  const course = await Course.update({_id: courseId}, {
+      $set:{
+        'author.name' : 'Juli update 2'
+      }
+  });
+}
+
 //createCourse('Node Course', new Author({ name: 'Mosh' }));
-updateAuthor('5afb6cb7e7f56f04aef4b358');
+//updateAuthor('5afb6cb7e7f56f04aef4b358');
+updateAuthor2('5afb6cb7e7f56f04aef4b358');
