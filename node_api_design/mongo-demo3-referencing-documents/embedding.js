9896f05fefb7c2d59e8d279c2dc09804600ad2d4
Julian Mesa <julianmesa@Julians-MacBook-Air.local>
2018-05-15 18:32:05 -0500

    added embedding document

---
+++ node_api_design/mongo-demo3-referencing-documents/embedding.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const authorSchema = new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String
+});
+
+const Author = mongoose.model('Author', authorSchema);
+
+const Course = mongoose.model('Course', new mongoose.Schema({
+  name: String,
+  author: authorSchema
+}));
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name, 
+    author
+  }); 
+  
+  const result = await course.save();
+  console.log(result);
+}
+
+async function listCourses() { 
+  const courses = await Course.find();
+  console.log(courses);
+}
+
+async function updateAuthor(courseId){
+  const course = await Course.findById(courseId);
+  course.author.name = 'Juli update';
+  course.save();
+}
+
+//createCourse('Node Course', new Author({ name: 'Mosh' }));
+updateAuthor('5afb6cb7e7f56f04aef4b358');
