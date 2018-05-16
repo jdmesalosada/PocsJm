387fe9e71ee1c1b67c55e7fabeb06ce8cde3a849
Julian Mesa <julianmesa@Julians-MacBook-Air.local>
2018-05-15 20:25:25 -0500

    added vidly

---
+++ node_api_design/vidly/models/rental.js
@@ -0,0 +1,69 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const Rental = mongoose.model('Rental', new mongoose.Schema({
+    //El porque usamos un nuevo esquema customer aca y no reusamos el que esta ya definido
+    // es porque en el otro esquema podemos tener 50 propiedades y no queremos tener 
+    //todas esas propiedades aca dentro del Rental, sino solo las mas importantes.
+
+    customer: {
+        type: new mongoose.Schema({
+            name: {
+                type: String,
+                required: true,
+                minlength: 5,
+                maxlenght: 50
+            },
+            isGold: {
+                type: Boolean,
+                default: false
+            },
+            phone: {
+                type: String,
+                required: true,
+                minlength: 5,
+                maxlenght: 50
+            }
+        }),
+        required: true
+    },
+    movie: {
+        type: new mongoose.Schema({
+            title: {
+                type: String,
+                required: true,
+                trim: true,
+                minlength: 5,
+                maxlenght: 255
+            },
+            dailyRentalRate: {
+                type: Number,
+                required: true,
+                min: 0,
+                max: 255
+            }
+        }),
+        required: true
+    },
+    dateOut: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    dateReturned: {
+        type: Date
+    },
+    rentalFee: {
+        type: Number,
+        min: 0
+    }
+}));
+
+function validateRental(rental){
+    const schema = {
+        customerId: Joi.string().required(),
+        movieId: Joi.string().required()
+    };
+
+    return Joi.validate(rental, schema);
+}
---
+++ node_api_design/vidly/routes/movies.js
@@ -0,0 +1,33 @@
+const { Movie, validate } = require('../models/movie');
+const { Genre } = require('../models/genre');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const movies = await Movie.find().sort('name');
+    res.send(movies);
+})
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = await Genre.findById(req.body.genreId);
+    if (!genre) return res.status(400).send('Invalid genre.');
+
+    let movie = new Movie({
+        title: req.body.title,
+        genre: {
+            _id: genre._id,
+            name: genre.name
+        },
+        numberInStock: req.body.numberInStock,
+        dailyRentalRate: req.body.dailyRentalRate
+    });
+
+    movie = await movie.save();
+
+    res.send(movie);
+
+})
