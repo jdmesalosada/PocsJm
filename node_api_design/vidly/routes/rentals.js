86e0d2bc511c0f4210991471b5cee17a73754004
Julian Mesa <julianmesa@Julians-MacBook-Air.local>
2018-05-15 21:21:59 -0500

    added fawn to manage transactions

--- node_api_design/vidly/routes/rentals.js
+++ node_api_design/vidly/routes/rentals.js
@@ -4,6 +4,9 @@ const { Customer } = require('../models/customer');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+const Fawn = require('fawn');
+
+Fawn.init(mongoose);
 
 router.get('/', async (req, res) => {
     //Traemos las rentas ordenadas por fecha de salida de forma descendente por eso usamos el - antes
@@ -24,6 +27,8 @@ router.post('/', async (req, res) => {
 
     if (movie.numberInStock === 0) return res.status(400).send('Movie not in stock');
 
+
+
     let rental = new Rental({
         customer: {
             _id: customer._id,
@@ -37,11 +42,30 @@ router.post('/', async (req, res) => {
         }
     });
 
-    rental = await rental.save();
-    
-    movie.numberInStock--;
-    movie.save();
+    /*Aca podemos ver que tenemos dos operaciones de guardado separadas,
+    pero puede pasar que cuando guardemos el rental satifactoriamente, y se intente
+    guardar la pelicula, ocurra un error, por eso aca vamos a necesitar transactions.
+    Con esto podemos asegurarnos que ambas operaciones seran ejecutadas y actualizaras los datos
+    en la BD, o en caso de que una no funcione ninguna de las dos sea aplicada.
+    En mongoose no se tiene este concepto pero existe una libreria que nos permite simular este concepto.
+    */
+    /*rental = await rental.save();
 
-    res.send(rental);
+    movie.numberInStock--;
+    movie.save();*/
+
+    try {
+        //Aca con fawn estamos ejecutando esta transaccion.
+        new Fawn.Task()
+            .save('rentals', rental)
+            .update('movies', { _id: movie._id }, {
+                $inc: { numberInStock: -1 }
+            })
+            .run();
+        res.send(rental);
+    }
+    catch (ex) {
+        res.status(500).send('Something failed');
+    }
 
 });
