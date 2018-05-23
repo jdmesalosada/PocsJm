0eaacb98d1d84dd765379d813fcf91625053f24c
Julian Mesa <julianmesa@Julians-MacBook-Air.local>
2018-05-22 19:59:21 -0500

    added jest

---
+++ node_api_design/testing-demo/tests/lib.test.js
@@ -0,0 +1,8 @@
+
+
+//test es una función que viene con jest.
+//el primer argumento es el nombre del test.
+//el segundo argumento es una funcion donde implementamos nuestro test
+test('Our first test', () => {
+throw new Error('some');
+});
