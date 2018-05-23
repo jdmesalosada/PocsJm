1f2213de52a966c95a0f56df79a1941caa0b9d0b
Julian Mesa <julianmesa@Julians-MacBook-Air.local>
2018-05-22 20:11:03 -0500

    added lib tests

--- node_api_design/testing-demo/tests/lib.test.js
+++ node_api_design/testing-demo/tests/lib.test.js
@@ -1,8 +1,19 @@
-
+const lib = require('../lib');
 
 //test es una función que viene con jest.
 //el primer argumento es el nombre del test.
 //el segundo argumento es una funcion donde implementamos nuestro test
-test('Our first test', () => {
-throw new Error('some');
+test('absolute - should return a positive number if input is positive', () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+});
+
+test('absolute - should return a positive number if input is negative', () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+});
+
+test('absolute - should return 0 if input is 0', () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
 });
