b9649526f5faf1993d9933a46c57a2e01a019c9d
Julian Mesa <julianmesa@Julians-MacBook-Air.local>
2018-10-14 20:54:08 -0500

    added combinable matcher

---
+++ matchers/src/main/java/matchers/combinable/HasName.java
@@ -0,0 +1,29 @@
+package matchers.combinable;
+
+import model.User;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class HasName {
+
+    public static Matcher<User> equal(final User expectedUser) {
+        return new TypeSafeMatcher<User>() {
+            @Override
+            protected boolean matchesSafely(User actualUser) {
+                return actualUser.getName().equals(expectedUser.getName());
+            }
+
+            public void describeTo(Description description) {
+                {
+                    description.appendText("Name should be: " + expectedUser.getName());
+                }
+            }
+
+            public void describeMismatchSafely(final User actualUser, final Description mismatchDescription) {
+                mismatchDescription.appendText(" was ").appendValue(actualUser.getName());
+            }
+
+        };
+    }
+}
